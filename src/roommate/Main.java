600f97ce42cf74e812ffc26fd105ed893639e74c
wellseekogi <es2006tiger@gmail.com>
2026-05-29 22:06:32 +0900

    chore: update main default input

--- src/roommate/Main.java
+++ src/roommate/Main.java
@@ -13,23 +13,43 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        String inputPath = args.length > 0 ? args[0] : "data/input_small.csv";
-        String outputPath = args.length > 1 ? args[1] : "result.txt";
+
+        String inputPath;
+        String outputPath;
+
+        if (args.length > 0) {
+            inputPath = args[0];
+        } else {
+            inputPath = "data/input_large.csv";
+        }
+
+        if (args.length > 1) {
+            outputPath = args[1];
+        } else {
+            outputPath = "result.txt";
+        }
 
         try {
             CsvReaderImpl reader = new CsvReaderImpl();
             List<Person> people = reader.read(inputPath);
 
             HardFilterImpl filter = new HardFilterImpl();
+
             DistanceRegistry registry = new DistanceRegistry();
+
             PenaltyCalculatorImpl calculator = new PenaltyCalculatorImpl(registry);
-            StableRoommatesMatcher matcher = new StableRoommatesMatcher(filter, calculator);
+
+            StableRoommatesMatcher matcher =
+                    new StableRoommatesMatcher(filter, calculator);
 
             List<MatchResult> results = matcher.match(people);
 
             ResultWriterImpl writer = new ResultWriterImpl();
+
             writer.printToConsole(results, people);
+
             writer.writeToFile(results, people, outputPath);
+
         } catch (RuntimeException e) {
             System.err.println("[오류] " + e.getMessage());
             System.exit(1);
